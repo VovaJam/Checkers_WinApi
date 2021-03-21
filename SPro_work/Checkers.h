7b9c2705a3cb5e5bbd038550413023e12f100ef2
vladimirsleep20@gmail.com <vladimirsleep20@gmail.com>
2021-03-21 22:24:39 +0200

    Start checkers class

--- SPro_work/Checkers.h
+++ SPro_work/Checkers.h
@@ -4,17 +4,30 @@
 #define DRAWCALL_H
 #endif
 
+#include <vector>
+typedef std::pair<int, int> Coordinates;
+
 enum CellState { NONE, CHOOSEN, ATTACKABLE };
+enum Color { BLACK, WHITE };
 
 struct Cell
 {
 	bool isFree = true;
-	bool CheckColor;
 	CellState state = NONE;
+	Checker* checker = nullptr;
 };
 
 extern Cell board[10][10];
 
+class Checker
+{
+	Color color;
+	int x, y;
+public:
+	bool CanAttack();
+	std::vector<Coordinates> getAttackablePoints();
+
+};
 
 class Game
 {
